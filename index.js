519b18773817011f3edf7a866e4294413aaf2bc3
Harry Kemefa <harry@uberweds.com.ng>
2022-02-02 12:28:20 +1345

    add comments to code

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+// IMPORT EXPRESS
 const express = require("express");
+// ITIALIZE
 const app = express();
+
+// IMPORT  MONGOOSE
 const mongoose = require("mongoose");
+
+// IMPORT DOTENV.
 const dotenv = require("dotenv");
 dotenv.config();
 
+// CONNECT TO MONGODB
 mongoose
  .connect(process.env.MONGO_URL)
  .then(() => { console.log("DB connection success!"); })
  .catch((err) => {console.log(err)});
 
+ 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!!");
 })
